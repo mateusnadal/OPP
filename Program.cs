dc941c7d160e859e01cfa04c63804cf57d3b6286
Mateus Nadal <nadalmateus@outlook.com>
2021-06-17 15:01:17 -0300

    Resolvendo um problema sem orientação a objetos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace OOP
 {
@@ -6,7 +7,42 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do trigangulo X");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do trigangulo Y");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+
+            Console.WriteLine("Area de x =" + areaX.ToString("f4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de y =" + areaY.ToString("f4", CultureInfo.InvariantCulture));
+
+
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("A area x é maior");
+            }
+            else
+            {
+                Console.WriteLine("A area x é maior");
+
+            }
+
+
         }
     }
 }
